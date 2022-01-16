bb724795524b58830caf6240e4493fe03c371e9a
domidodo <dhitschmann@live.de>
2022-01-16 13:18:17 +0100

    Movie hover-style to global .js

--- RoomAndResourcesScheduler/wwwroot/js/site.js
+++ RoomAndResourcesScheduler/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("*[onclick]").hover(
+        function () {
+            $(this).addClass('shadow').css('cursor', 'pointer');
+        }, function () {
+            $(this).removeClass('shadow');
+        });
+}
