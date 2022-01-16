7bacd49c9a84fd8798c9a26cec494ecf092b099f
domidodo <dhitschmann@live.de>
2022-01-16 13:02:07 +0100

    Update Models in WebUi

--- RoomAndResourcesScheduler/Models/Event.cs
+++ RoomAndResourcesScheduler/Models/Event.cs
@@ -3,8 +3,8 @@
     public class Event
     {
         public int Id { get; set; }
-        public int ResourceId { get; set; }
-        public int OrganizerId { get; set; }
+        public int ResourceId { get; set; } = 0;
+        public int OrganizerId { get; set; } = 0;
         public bool IsPrivate { get; set; } = false;
         public bool EnableJoinNotification { get; set; } = true;
         public string Name { get; set; } = "";
@@ -12,7 +12,11 @@
         public string Description { get; set; } = "";
         public List<int> VisitorIds { get; set; } = new List<int>();
         public int MaxVisitorCount { get; set; }
-        public List<TimePeriod> Schedule { get; set; } = new List<TimePeriod>();
+        public List<int> TimePeriodIds { get; set; } = new List<int>();
         public List<string> Tags { get; set; } = new List<string>();
+        public Resource? Resource { get; set; } = null;
+        public User? Organizer { get; set; } = null;
+        public List<User>? Visitors { get; set; } = null;
+        public List<TimePeriod>? Schedule { get; set; } = null;
     }
 }
--- RoomAndResourcesScheduler/Models/TimePeriod.cs
+++ RoomAndResourcesScheduler/Models/TimePeriod.cs
@@ -2,7 +2,8 @@
 {
     public class TimePeriod
     {
-        public int Id { get; set; }
+        public int Id { get; init; } = 0;
+        public int? EventId { get; set; }
         public DateTime From { get; set; } 
         public DateTime To { get; set; }
     }
