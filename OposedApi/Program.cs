505a75ad505efc93fc049841b519ec2c5e18f02c
domidodo <dhitschmann@live.de>
2022-05-26 10:42:30 +0200

    Update Program.cs

--- OposedApi/Program.cs
+++ OposedApi/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Rooms and resources API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Oposed API", Version = "v1" });
     c.EnableAnnotations();
     c.OperationFilter<AuthFilter>();
 });
