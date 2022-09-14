7f0af078d71a41272c4a625853f4de1bb0122b10
domidodo <dhitschmann@live.de>
2022-09-14 13:40:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+docker-compose down
+
+git pull
 
 docker build -f "OposedApi/Dockerfile" --force-rm -t oposed_api .
 docker build -f "Oposed/Dockerfile" --force-rm -t oposed_web .
