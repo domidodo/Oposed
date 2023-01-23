c20451c6c87e513ebabf93e9db5b9f2bf1bc7d7f
Dominik Hitschmann <dominik.hitschmann@finanzen.net>
2023-01-23 15:00:49 +0100

    Add Demo data

--- demo/startDemo.bat
+++ demo/startDemo.bat
@@ -5,3 +5,5 @@ docker build -f "../OposedApi/Dockerfile" --force-rm -t oposed_api ../
 docker build -f "../Oposed/Dockerfile" --force-rm -t oposed_web ../
 
 docker-compose up -d
+
+start "" http://localhost:8080
