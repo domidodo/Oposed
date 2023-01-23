b70061e9a2e9097d4a423b3a17a3668426512d3e
Dominik Hitschmann <dominik.hitschmann@finanzen.net>
2023-01-23 22:03:53 +0100

    no message

--- example/start.sh
+++ example/start.sh
@@ -3,7 +3,7 @@ docker-compose down
 
 git pull
 
-docker build -f "../src/OposedApi/Dockerfile" --force-rm -t oposed_api ../src/
-docker build -f "../src/Oposed/Dockerfile" --force-rm -t oposed_web ../src/
+docker build -f ../src/OposedApi/Dockerfile --force-rm -t oposed_api ../src/
+docker build -f ../src/Oposed/Dockerfile --force-rm -t oposed_web ../src/
 
 docker-compose up -d
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker-compose down
 
-docker build -f "src/OposedApi/Dockerfile" --force-rm -t oposed_api ./src/
-docker build -f "src/Oposed/Dockerfile" --force-rm -t oposed_web ./src/
+docker build -f ./src/OposedApi/Dockerfile --force-rm -t oposed_api ./src/
+docker build -f ./src/Oposed/Dockerfile --force-rm -t oposed_web ./src/
 
 docker-compose up -d
